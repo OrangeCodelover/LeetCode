21ec5fb67330892e4a516e5a0ecdfa4002bb63b7
JimmyLinLincoln <118079800+JimmyLinLincoln@users.noreply.github.com>
2024-04-15 22:08:04 +1200

    Set up for 2.

---
+++ 2. Longest Substring Without Repeating Characters/2.Longest Substring Without Repeating Characters_2.py
@@ -0,0 +1,22 @@
+
+def lengthOfLongestSubstring(s: str) -> int:
+    
+
+#EX_1
+s = "abcabcbb"
+a = lengthOfLongestSubstring(s)
+print(a)
+# 3
+
+#EX_2
+s = "bbbbb"
+target = 6   
+b = lengthOfLongestSubstring(s)
+print(b)
+# [1,2]
+
+#EX_3
+s = "pwwkew"
+c = lengthOfLongestSubstring(s)
+print(c)
+# [0,1]
