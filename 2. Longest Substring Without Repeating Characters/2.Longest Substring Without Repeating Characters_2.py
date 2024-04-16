6cc8df4710ab922280d03b5d9aec6819eb3fb4ee
JimmyLinLincoln <118079800+JimmyLinLincoln@users.noreply.github.com>
2024-04-16 22:07:42 +1200

    WIP

--- 2. Longest Substring Without Repeating Characters/2.Longest Substring Without Repeating Characters_2.py
+++ 2. Longest Substring Without Repeating Characters/2.Longest Substring Without Repeating Characters_2.py
@@ -1,6 +1,23 @@
 
 def lengthOfLongestSubstring(s: str) -> int:
-    
+    max = 0
+    start_index = 0
+    List = ''
+    for i, n in enumerate(s):
+        if n not in List:
+            List = List +n
+            print("List:",List)
+            if len(List) >= max:
+                max = len(List)
+        
+        else:
+            List = List +n
+            List = List[1:]
+            print(List)
+    return max
+            
+
+
 
 #EX_1
 s = "abcabcbb"
@@ -9,14 +26,33 @@ print(a)
 # 3
 
 #EX_2
-s = "bbbbb"
-target = 6   
+s = "bbbbb" 
 b = lengthOfLongestSubstring(s)
 print(b)
-# [1,2]
+# 1
 
 #EX_3
 s = "pwwkew"
 c = lengthOfLongestSubstring(s)
 print(c)
-# [0,1]
+# 3
+
+
+#EX_4
+s = " "
+d = lengthOfLongestSubstring(s)
+print(d)
+# 1
+
+
+#EX_5
+s = "au"
+c = lengthOfLongestSubstring(s)
+print(c)
+# 2
+
+#EX_6
+s = "dvdf"
+c = lengthOfLongestSubstring(s)
+print(c)
+# 3
