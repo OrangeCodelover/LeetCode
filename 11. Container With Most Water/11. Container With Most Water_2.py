3a0c647f7252cbe32d83bc87ef64a9cd615f358f
JimmyLinLincoln <118079800+JimmyLinLincoln@users.noreply.github.com>
2024-04-21 00:45:14 +1200

    11. done

--- 11. Container With Most Water/11. Container With Most Water_2.py
+++ 11. Container With Most Water/11. Container With Most Water_2.py
@@ -2,12 +2,17 @@ from typing import List
 
 def maxArea(height: List[int]) -> int:
     max_Area = 0
-    for i, n in enumerate(height):
-        while i < len(height):
-            if (len(height) - i)*n > max_Area:
-                max_Area = (len(height) - i)*n
-                print("Max_Area:", max_Area)
+    i = 0
+    j = len(height)-1
+    while i<=j:
+        if (j-i)*(min(height[i],height[j])) > max_Area:
+            max_Area = (j-i)*(min(height[i],height[j]))
+
+        if height[i] > height[j]:
+            j -= 1
+        else:
             i += 1
+
     return max_Area
 
 #EX_1
@@ -15,3 +20,9 @@ height = [1,8,6,2,5,4,8,3,7]
 a = maxArea(height)
 print(a)
 # 49
+
+#EX_2
+height = [1,1]
+a = maxArea(height)
+print(a)
+# 1
