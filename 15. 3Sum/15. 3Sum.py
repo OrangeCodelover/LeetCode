494d2164be43ba268098ae76de649969847df2d9
JimmyLinLincoln <118079800+JimmyLinLincoln@users.noreply.github.com>
2024-04-22 01:52:09 +1200

    15. done

--- 15. 3Sum/15. 3Sum.py
+++ 15. 3Sum/15. 3Sum.py
@@ -5,6 +5,8 @@ def threeSum(nums: List[int]) -> List[List[int]]:
     # print("num:", nums)
     sum_zero = []
     for i,n in enumerate(nums):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue  # Skip duplicates
         # print("i:",i)
         j = i + 1
         k = len(nums) -1
@@ -13,15 +15,17 @@ def threeSum(nums: List[int]) -> List[List[int]]:
             # print("j :",j)
             # print("k :",k)
             if   n + nums[j] + nums[k] == 0:
-                newArray = [n,nums[j], nums[k]]
-                if newArray not in sum_zero:
-                    sum_zero.append(newArray) 
+                sum_zero.append([n,nums[j], nums[k]])
+                while j < k and nums[j] == nums[j + 1]:
+                    j += 1
+                while j < k and nums[k] == nums[k - 1]:
+                    k -= 1
+                k -= 1
                 j += 1
             elif n + nums[j] + nums[k] < 0:
                 j += 1
             elif n + nums[j] + nums[k] > 0:
-                k -= 1
-                
+                k -= 1                
     return sum_zero
 
 #EX_1
@@ -41,39 +45,3 @@ nums = [0,0,0]
 a = threeSum(nums)
 print(a)
 # [[0,0,0]]
-
-"""
-from typing import List
-
-class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        sum_zero = []
-        nums.sort()  # Sort the input list
-        n = len(nums)
-        
-        for i in range(n):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue  # Skip duplicates
-            
-            left = i + 1
-            right = n - 1
-            
-            while left < right:
-                total = nums[i] + nums[left] + nums[right]
-                if total == 0:
-                    sum_zero.append([nums[i], nums[left], nums[right]])
-                    # Skip duplicates
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-                    left += 1
-                    right -= 1
-                elif total < 0:
-                    left += 1
-                else:
-                    right -= 1
-        
-        return sum_zero
-
-"""
