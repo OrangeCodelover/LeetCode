72faa79de35f47b32cde71740e921801b955c226
OrangeCodeLover <linenjimmy@gmail.com>
2024-09-12 20:08:20 +1200

    set up questtion 2215

---
+++ Blind 75/2215. Find the Difference of Two Arrays/test_find_the_difference_of_two_arrays.py
@@ -0,0 +1,15 @@
+import unittest
+
+from find_the_difference_of_two_arrays import Solution
+
+class testFindDifference(unittest.TestCase):
+    def test_sample_1(self):
+        nums1 = [1,2,3]
+        nums2 = [2,4,6]
+        expected = [[1,3],[4,6]]
+        solution_instance = Solution()
+        output = solution_instance.findDifference(nums1,nums2)
+        self.assertEqual(output, expected)
+
+if __name__ == "__main__":
+    unittest.main()
